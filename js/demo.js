2d8106793f59df9a446f6aa3f44fa47a2f7656a0
Pankaj Gudlani <pankaj.gudlani@gmail.com>
2014-06-10 14:30:24 +0530

    Added controls with left and right keys also

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,7 @@
+var k;
 (function(){
   // Paginated scroll
-  new KineticScroll(document.getElementById('paginated'), {
+    new KineticScroll(document.getElementById('paginated'), {
     vertical: false,
     horizontal: true,
     horizontalBar: false,
@@ -9,6 +10,7 @@
       var html = '';
       for (var i = 0; i < pageCount; i++) {
         if (i === currentPage) {
+          k = i;
           html += '<span class="current"></span>';
         } else {
           html += '<span></span>';
@@ -22,6 +24,63 @@
         page.className = '';
       });
       pages[currentPage].className = 'current';
+      k = currentPage;
     }
   });
 })();
+
+
+
+var slideCount = $('#paginated ul li').length;
+var slideWidth = $('#paginated ul li').width();
+var slideHeight = $('#paginated ul li').height();
+var paginatedUlWidth = slideCount * slideWidth;
+        
+        
+    
+
+function moveLeft() {
+    if(k==0)
+    {
+        $('#paginated ul li:last-child').prependTo('#paginated ul');
+        $('#paginated ul').css('left', -500);
+        $('#paginated ul').animate({
+            left: + 0
+        }, 200);
+    }
+    else
+    {
+        $('#paginated ul').animate({
+            left: + slideWidth
+          }, 200, function(){
+            $('#paginated ul li:last-child').prependTo('#paginated ul');
+            $('#paginated ul').css('left', '');
+        });
+    }
+};
+
+function moveRight() {
+    if(k==slideCount-1)
+    {
+        $('#paginated ul li:first-child').appendTo('#paginated ul');
+        $('#paginated ul').css('left', +500);
+        $('#paginated ul').animate({
+            left: + 0
+        }, 200);
+    }
+    else
+    {
+        $('#paginated ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#paginated ul li:first-child').appendTo('#paginated ul');
+            $('#paginated ul').css('left', '');
+        });
+    }
+};
+
+
+$(document).keydown(function(e){
+    if(e.which==37) moveLeft();
+    else if(e.which==39) moveRight();
+});
