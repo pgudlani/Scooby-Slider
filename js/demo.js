13083d2f4020e90ce5aa21ab9d7553d76e5188e4
Pankaj Gudlani <pankaj.gudlani@gmail.com>
2014-06-11 15:52:48 +0530

    Added images and custom trigger when page changes

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,12 @@
-var k;
+var page_now, k, l;
+
+$('#paginated ul li').each(function(index){
+    $(this).data('index',index);
+});
+
 (function(){
   // Paginated scroll
-    new KineticScroll(document.getElementById('paginated'), {
+    l =new KineticScroll(document.getElementById('paginated'), {
     vertical: false,
     horizontal: true,
     horizontalBar: false,
@@ -17,14 +22,11 @@ var k;
         }
       }
       document.getElementById('pager').innerHTML = html;
+      jQuery(document).trigger('pageChange');
     },
     onCurrentPageChange: function(currentPage) {
-      pages = document.querySelectorAll('#pager > span');
-      Array.prototype.slice.apply(pages).forEach(function(page) {
-        page.className = '';
-      });
-      pages[currentPage].className = 'current';
       k = currentPage;
+      jQuery(document).trigger('pageChange');
     }
   });
 })();
@@ -46,7 +48,9 @@ function moveLeft() {
         $('#paginated ul').css('left', -500);
         $('#paginated ul').animate({
             left: + 0
-        }, 200);
+          }, 200, function(){
+           jQuery(document).trigger('pageChange');
+        });
     }
     else
     {
@@ -56,6 +60,7 @@ function moveLeft() {
           }, 200, function(){
             $('#paginated ul li:last-child').prependTo('#paginated ul');
             $('#paginated ul').css('left', '');
+            jQuery(document).trigger('pageChange');
         });
     }
 };
@@ -68,7 +73,9 @@ function moveRight() {
         $('#paginated ul').css('left', +500);
         $('#paginated ul').animate({
             left: + 0
-        }, 200);
+          }, 200, function(){
+           jQuery(document).trigger('pageChange');
+        });
     }
     else
     {
@@ -78,6 +85,7 @@ function moveRight() {
         }, 200, function () {
             $('#paginated ul li:first-child').appendTo('#paginated ul');
             $('#paginated ul').css('left', '');
+            jQuery(document).trigger('pageChange');
         });
     }
 };
@@ -87,3 +95,17 @@ $(document).keydown(function(e){
     if(e.which==37) moveLeft();
     else if(e.which==39) moveRight();
 });
+
+$(document).on('pageChange', function(){
+  page_now = $('#paginated ul li:nth-child('+(k+1)+')').data('index');
+  console.log(page_now);
+  pages = document.querySelectorAll('#pager > span');
+  Array.prototype.slice.apply(pages).forEach(function(page) {
+    page.className = '';
+  });
+  pages[page_now].className = 'current';
+});
+
+var data_shown = '#'+$('#paginated ul').data('div');
+
+
